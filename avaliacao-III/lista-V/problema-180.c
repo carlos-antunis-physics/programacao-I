c264a3b1ae5c9835dd38659eb6252d1895517616
carlos antunis physics <carlos.bonfim@fis.ufal.br>
2025-03-30 19:31:02 -0300

    Minor fixes

--- avaliacao-III/lista-V/problema-180.c
+++ avaliacao-III/lista-V/problema-180.c
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 
-void excede_capacidade(int, int);
+int excede_capacidade(int, int);
 
 int main() {
     unsigned N, c;
@@ -16,15 +16,18 @@ int main() {
     //  e sua capacidade maxima
     scanf("%u%u", &N, &c);
     //  executar o processamento dos andares
-    excede_capacidade(N, c);
+    if (excede_capacidade(N, c)) {
+        printf("S\n");
+    } else {
+        printf("N\n");
+    }
     return 0;
 }
 
-void excede_capacidade(int andares, int capacidade) {
+int excede_capacidade(int andares, int capacidade) {
     // se completar os andares sem exceder a capacidade
     if (!andares) {
-        printf("N\n");
-        return;
+        return 0;
     }
     unsigned saida, entrada;
     //  leia a quantidade de pessoas que saem e entram no andar
@@ -32,9 +35,8 @@ void excede_capacidade(int andares, int capacidade) {
     capacidade += (saida - entrada);    // atualizar a capacidade
     //  se a capacidade do elevador for excedida num andar
     if (capacidade < 0) {
-        printf("S\n");
-        return;
+        return 1;
     }
     //  requira a leitura do proximo andar
-    excede_capacidade(andares - 1, capacidade);
+    return excede_capacidade(andares - 1, capacidade);
 }
