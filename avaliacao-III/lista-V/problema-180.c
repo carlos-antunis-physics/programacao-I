304e1f30686d033edad213a09b662036d1d05389
carlos antunis physics <carlos.bonfim@fis.ufal.br>
2025-03-30 19:25:15 -0300

    Update problem set V
    [add] avaliacao-III/lista-V/problema-180.c

---
+++ avaliacao-III/lista-V/problema-180.c
@@ -0,0 +1,40 @@
+/*
+    Autor: Carlos Antunis Bonfim da Silva Santos
+        Github: https://github.com/carlos-antunis-physics/
+
+    Problema: 180 - Elevador
+        Link: https://www.thehuxley.com/problem/180
+*/
+
+#include <stdio.h>
+
+int excede_capacidade(int, int);
+
+int main() {
+    unsigned N, c;
+    //  ler quantidade de processos o elevador ira executar
+    //  e sua capacidade maxima
+    scanf("%u%u", &N, &c);
+    //  executar o processamento dos andares
+    printf("%c\n", excede_capacidade(N, c) ? "S" : "N");
+    return 0;
+}
+
+int excede_capacidade(int andares, int capacidade) {
+    // se completar os andares sem exceder a capacidade
+    if (!andares) {
+        printf("N\n");
+        return 0;
+    }
+    unsigned saida, entrada;
+    //  leia a quantidade de pessoas que saem e entram no andar
+    scanf("%u%u", &saida, &entrada);
+    capacidade += (saida - entrada);    // atualizar a capacidade
+    //  se a capacidade do elevador for excedida num andar
+    if (capacidade < 0) {
+        printf("S\n");
+        return 1;
+    }
+    //  requira a leitura do proximo andar
+    return excede_capacidade(andares - 1, capacidade);
+}
