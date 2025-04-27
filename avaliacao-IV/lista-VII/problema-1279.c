396d124d56ef073de5e7cac7daf0b6b88c74d0f3
carlos antunis physics <carlos.bonfim@fis.ufal.br>
2025-04-27 03:26:04 -0300

    Update problem set VII
    [add] avaliacao-IV/lista-VII/problema-1279.c

---
+++ avaliacao-IV/lista-VII/problema-1279.c
@@ -0,0 +1,39 @@
+/*
+    Autor: Carlos Antunis Bonfim da Silva Santos
+        Github: https://github.com/carlos-antunis-physics/
+
+    Problema: 1291 - Brincando com matrizes
+        Link: https://www.thehuxley.com/problem/1291
+*/
+
+#include <stdio.h>
+
+void somar_matrizes_quadradas(unsigned);
+
+int main()
+{
+    unsigned N;
+    scanf("%u", &N);
+    if (N == 0)
+    {
+        printf("Vazia\n");
+        return 0;
+    }
+    somar_matrizes_quadradas(N * N);
+    return 0;
+}
+
+void somar_matrizes_quadradas(unsigned entradas)
+{
+    int matriz[entradas];
+    int elemento;
+    for (unsigned i = 0; i < entradas; i++)
+    {
+        scanf("%d", &matriz[i]);
+    }
+    for (unsigned i = 0; i < entradas; i++)
+    {
+        scanf("%d", &elemento);
+        printf("%d\n", matriz[i] + elemento);
+    }
+}
